93e8a90282eb33718818931fd855c1b845135248
Mike Cebulski <mikecebul@gmail.com>
2024-06-11 21:48:12 -0400

    Update revalidate.ts

--- src/payload/utilities/revalidate.ts
+++ src/payload/utilities/revalidate.ts
@@ -16,6 +16,10 @@ export const revalidate = async (args: {
 
     const res = await fetch(url)
 
+    if (collection === "posts") {
+      await fetch(baseUrl + '&path=' + '/posts')
+    }
+
     if (res.ok) {
       switch (type) {
         case 'path':
